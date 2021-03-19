42e74c7415cd62b5f83cb8e994048d3543646b6a
Daniel Medina <danielmedinaneira555@gmail.com>
2021-03-19 12:37:26 -0500

    Condense the information into a single table

--- app.R
+++ app.R
@@ -7,7 +7,7 @@ ui <- fluidPage(
   numericInput("limit", "Limit of articles", value = 50),
   numericInput("days_before", "Within the specified number of days", value = 365),
   actionButton("button_search", "Search!"),
-  textOutput("articles")
+  tableOutput("table")
 )
 
 server <- function(input, output, session) {
@@ -15,11 +15,11 @@ server <- function(input, output, session) {
   result <- eventReactive(input$button_search, {
     input$query %>% 
       query_to_pmid(limit = input$limit, days_before = input$days_before) %>% 
-      pmid_to_refs() %>% 
+      pmid_to_refs() %>%
       refs_to_freqs()
   })
-
-  output$articles <- renderText(result())
+  
+  output$table <- renderTable(tibble(result(), Article = pmid_to_article(result()$PMID)))
   
 }
 
