30a33b7ab2d33c660378f5bd8213236173e405e0
Daniel Medina <danielmedinaneira555@gmail.com>
2021-03-19 13:29:30 -0500

    increase sleep time from 1/5 to 1/4 to give more margin and avoid problems with the server

--- functions.R
+++ functions.R
@@ -20,7 +20,7 @@ query_to_pmid <- function(query, limit = 50, days_before = 365) {
 pmid_to_refs <- function(pmid) {
   # obtain the PMIDs of the references from a list of PMIDs
   map(pmid, function(id) {
-    Sys.sleep(1/5)
+    Sys.sleep(1/4)
     base <- "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
     db <- "pubmed"
     url <- paste0(
